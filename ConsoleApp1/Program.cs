7fad9da4cad1af1701b5849aa7a4699b847edb4a
gitxgzh <463221420@qq.com>
2019-09-29 01:37:18 +0800

    create

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+using XSocket.Common;
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Socket socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            SocketInformation socket1;
+            
+           
+            IPAddress iP = new IPAddress(0x100007f);
+            IPEndPoint iPEnd = new IPEndPoint(0x0100007f, 8080);
+
+            Task a =  Task.Factory.StartNew(()=>
+           {
+               socket.Bind(iPEnd);
+               socket.Listen(10000);
+               socket.Accept();
+           }
+            );
+
+            socket.SafeClose();
+        
+        }
+    }
+}
---
+++ XSocket/Common/SocketExt.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+
+namespace XSocket.Common
+{
+    public static class SocketExt
+    {
+        public static void SafeClose(this Socket client)
+        {
+            if (client == null)
+            {
+                return;
+            }
+
+            try
+            {
+                if (client.Connected)
+                {
+                    client.Shutdown(SocketShutdown.Both);
+                }
+            }
+            catch  
+            { 
+            }
+
+            try
+            {
+                client.Close();
+            }
+            catch
+            {
+            }
+        }
+
+
+        public static int SendData(this Socket clinet, byte[] buffer)
+        {
+            return SendData(clinet, buffer, 0, buffer.Length);
+        }
+
+        /// <summary>
+        /// sends the data
+        /// </summary>
+        /// <param name="clinet"></param>
+        /// <param name="buffer"></param>
+        /// <param name="offset"></param>
+        /// <param name="length"></param>
+        /// <returns></returns>
+        public static int SendData(this Socket clinet, byte[] buffer, int offset, int length)
+        {
+            int sent = 0;
+            while (length > sent)
+            {
+                sent += clinet.Send(buffer, offset + sent, length - sent, SocketFlags.None);
+            }
+
+            return sent;
+        }
+    }
+}
